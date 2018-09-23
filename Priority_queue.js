ab08794b0e7669a6c630f64545cf106a9297b7b6
alvind1 <40304595+alvind1@users.noreply.github.com>
2018-09-22 20:24:24 -0400

    Update Priority_queue.js

--- Priority_queue.js
+++ Priority_queue.js
@@ -1,27 +1,24 @@
-function terms(success, n, name, definition){  //object dictionary for all the terms
-  this.scs = success; //Score out of 100, 100: mastered, 0: more repeats
-  this.num = n; //Index of 
-  this.term = name; //Term of flashcard
-	this.def = definition; 
+function terms(success, name, definition){ 
+  this.scs = success; //Out of 100
+  //this.num = n; //Index of dictionary
+  this.term = name; 
+  this.def = definition;
 }
 
-var dict = []; //Always stays the same order
-var sort_a = []; //Array to sort using priority queue
+var dict = []; //Always stays the same order. String-term key
+var sort_a = []; //Integer key. Updated sorted array
 
+dict[add.term] = new terms(2, add.term, add.def);
+sort_a[sort_a.length] = dict[add.term]; //Updating sorted array
 
-var y  = 0;
-while(y < 10){ //Getting the terms, y value is to be changed
-  dict[y] = new terms(10-y, y, "term"+y, "def"+y);
-  sort_a[y] = dict[y];
-  y++;
-}
+dict[add1.term] = new terms(0, add1.term, add1.def);
+sort_a[sort_a.length] = dict[add1.term]; //Updating sorted array
 
-for(i=0; i<dict.length; i++){ //Printing stuff out
-  console.log(sort_a[i]); //
-}
-console.log(); //
+c(dict);
+c(sort_a);
+c();
 
-sort_a.sort(function cmp(a, b){ //Priority sorting queue
+sort_a.sort(function cmp(a, b){ //Sort sorted array
   return a.scs-b.scs;
 });
 
